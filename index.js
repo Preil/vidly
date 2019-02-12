1072328424b44d5f0993271eda8722200e97fd80
preil <preil2000@gmail.com>
2019-02-12 18:06:16 +0200

    User model, routes added.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
+const users = require('./routers/users');
 const express = require('express');
 const app = express();
 
@@ -17,6 +18,7 @@ app.use('/api/genres', genres);
 app.use('/api/movies', movies);
 app.use('/api/customers', customers);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (res, req) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let user = await User.findOne({email: req.body.email});
+    if(user) return res.status(400).send('User with this email already registered.');
+
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+
+    });
+});
+
+module.exports.router;
