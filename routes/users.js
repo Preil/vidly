d12fa864e0244700f8d5a5b7b0bcbd8a9e2a5988
preil <preil2000@gmail.com>
2019-02-12 18:17:16 +0200

    Lodash lib added. User route refactored.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const {User, validate} = require('../models/user');
+const _ = require('lodash');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -7,14 +8,13 @@ router.post('/', async (res, req) => {
     const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     let user = await User.findOne({email: req.body.email});
-    if(user) return res.status(400).send('User with this email already registered.');
+    if (user) return res.status(400).send('User with this email already registered.');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+    // using lodash to get only specified properties from the object
+    user = new User(_.pick(req.body), ['name'], ['email'], ['password']);
+    await user.save();
+    res.send(user);
 
-    });
 });
 
 module.exports = router;
